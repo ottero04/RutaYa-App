153f1e41da1c28136300dc2be7a1c4ae1c089302
Andres Otero <robinsonotero70@gmail.com>
2026-05-21 16:47:54 -0500

    add: hook useResponsive

---
+++ hooks/useResponsive.ts
@@ -0,0 +1,21 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+export function useResponsive() {
+  const [isMobile, setIsMobile] = useState(false)
+  const [isTablet, setIsTablet] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+      setIsTablet(window.innerWidth >= 768 && window.innerWidth < 1024)
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return { isMobile, isTablet }
+}
